80d880ba30e9189869aa1ba790cd53b888d9dd4e
Taba <elena.tabakov@gmail.com>
2024-08-11 02:14:15 +0300

    Change router

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter  basename="/react-burger">
         <App />
       </BrowserRouter>
     </React.StrictMode>
